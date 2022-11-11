228d1e654db6f1006d793b250b5e3e424e921c8b
Saleh Shmali <114103559+SShmali@users.noreply.github.com>
2022-11-11 08:55:48 +0000

    build fast api code

---
+++ app/api.py
@@ -0,0 +1,34 @@
+import spacy
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+en_core_web = spacy.load("en_core_web_sm")
+app = FastAPI(tags=['sentence'])
+
+class Input(BaseModel):
+    sentence: str
+
+@app.post("/analyze_text")
+def get_text_characteristics(sentence_input: Input):
+    document = en_core_web(sentence_input.sentence)
+    output_array = []
+    for token in document:
+        output = {
+            "Index": token.i, "Token": token.text, "Tag": token.tag_, "POS": token.pos_,
+            "Dependency": token.dep_, "Lemma": token.lemma_, "Shape": token.shape_,
+            "Alpha": token.is_alpha, "Is Stop Word": token.is_stop
+        }
+        output_array.append(output)
+    return {"output": output_array}
+
+@app.post("/entity_recognition")
+def get_entity(sentence_input: Input):
+    document = en_core_web(sentence_input.sentence)
+    output_array = []
+    for token in document.ents:
+        output = {
+            "Text": token.text, "Start Char": token.start_char,
+            "End Char": token.end_char, "Label": token.label_
+        }
+        output_array.append(output)
+    return {"output": output_array}
